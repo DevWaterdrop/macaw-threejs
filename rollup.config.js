9b7650b1df992913d66d01703a6190d7732f2e4f
DevWaterdrop <dev.waterdrop@gmail.com>
2022-04-12 17:50:54 +0200

    Refactor: simplified rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,121 +1,33 @@
-import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
-import pluginCommonjs from "@rollup/plugin-commonjs";
 import pluginNodeResolve from "@rollup/plugin-node-resolve";
-import { babel } from "@rollup/plugin-babel";
-import * as path from "path";
-import pkg from "./package.json";
-import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
 import del from "rollup-plugin-delete";
 
-//* --- SETTINGS ---
-const ENABLE_CJS = false;
-
-//* CONSTANTS
-const TYPES = {
-	core: "core",
-	effect: "effect"
-};
-const MODULE_NAMES = { core: "macaw-threejs" };
-const INPUTS = { core: "src/index.ts" };
-
-//* PKG
-const { author, version, license } = pkg;
-
-//* BANNER
-const banner = `/**
-	* @license
-	* author: ${author}
-	* ${MODULE_NAMES.core}.js v${version}
-	* Released under the ${license} license.
-	*/
-
-//	* ██    ██ ██   ██ ██████   █████  ██ ███    ██ ███████ 
-//	* ██    ██ ██  ██  ██   ██ ██   ██ ██ ████   ██ ██      
-//	* ██    ██ █████   ██████  ███████ ██ ██ ██  ██ █████   
-//	* ██    ██ ██  ██  ██   ██ ██   ██ ██ ██  ██ ██ ██      
-//	*  ██████  ██   ██ ██   ██ ██   ██ ██ ██   ████ ███████
-`;
-
 //* CONFIG
-
-const terserOptions = { format: { comments: false, preamble: banner } };
-const plugins = [
-	typescript(),
-	pluginCommonjs({
-		extensions: [".js", ".ts"]
-	}),
-	babel({
-		babelHelpers: "bundled",
-		configFile: path.resolve(__dirname, ".babelrc.cjs")
-	}),
-	pluginNodeResolve({
-		browser: false
-	}),
-	terser(terserOptions)
-];
-const external = [
-	...Object.keys(pkg.dependencies || {}),
-	...Object.keys(pkg.devDependencies || {})
-];
-
-const createOutputFile = ({ ending, type, fileName }) => {
-	let base = "build";
-	if (type === TYPES.effect) base = "build/effects";
-
-	return `${base}/${fileName}.${ending}`;
+const terserOptions = {
+	format: { comments: false }
 };
 
-const createOutput = ({ type, fileName }) => ({
-	es: [{ file: createOutputFile({ ending: "module.js", type, fileName }), format: "es" }],
-	cjs: [{ file: createOutputFile({ ending: "cjs", type, fileName }), format: "cjs" }]
-});
-
-const createConfig = ({ input, type, fileName }) => {
-	const { es: outputES, cjs: outputCJS } = createOutput({ type, fileName });
-
-	const es = { input, output: outputES, external, plugins };
-	const cjs = { input, output: outputCJS, external, plugins };
-
-	const config = [es];
-	if (ENABLE_CJS) config.push(cjs);
-
-	return config;
+const deleteBuildFolder = {
+	input: "empty_input.js",
+	plugins: [del({ targets: "build" })]
 };
 
-// fs/fs-extra doesn't work because of readdir is async :(
-const effectNames = ["click_wave", "scroll_wave_top", "scroll_wrap_under"];
-
-const effects = effectNames
-	.map((fileName) => {
-		const input = `src/effects/${fileName}.ts`;
-		return createConfig({ input, type: TYPES.effect, fileName });
-	})
-	.flat();
-
-const core = createConfig({ input: INPUTS.core, type: TYPES.core, fileName: MODULE_NAMES.core });
-
-export default [
-	// Delete build folder
-	{
-		input: "empty_input.js",
-		plugins: [del({ targets: "build" })]
+const core = {
+	input: "src/index.ts",
+	output: {
+		dir: "build",
+		format: "es",
+		preserveModules: true,
+		preserveModulesRoot: "src"
 	},
+	plugins: [
+		typescript(),
+		pluginNodeResolve({
+			browser: false
+		}),
+		terser(terserOptions)
+	]
+};
 
-	// Create configs
-	...core,
-	...effects,
-
-	// Types
-	{
-		input: "build/src/index.d.ts",
-		output: [{ file: "build/index.d.ts", format: "esnext" }],
-		plugins: [dts()]
-	},
-
-	// Delete unused types
-	{
-		input: "empty_input.js",
-		plugins: [del({ targets: ["build/src", "build/effects/src"] })]
-	}
-];
+export default [deleteBuildFolder, core];
